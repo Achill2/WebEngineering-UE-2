ca7778fff6ad6ca449ea189ab9363f9f01bbb61a
Fabian Filip <fabian.filip@inode.at>
2014-04-16 00:23:56 +0200

    web.xml servlet-path ausgebessert, servlet test-aufruf mit test-html inhalt

--- src/main/java/at/ac/tuwien/big/we14/lab2/servlet/BigQuizServlet.java
+++ src/main/java/at/ac/tuwien/big/we14/lab2/servlet/BigQuizServlet.java
@@ -1,11 +1,23 @@
 package at.ac.tuwien.big.we14.lab2.servlet;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class BigQuizServlet extends HttpServlet {
 	
 	
-	
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		// test
+		res.setContentType("text/html");
+		PrintWriter out = res.getWriter();
+		out.println("<h1>test</h1>");
+		
+	}
 	
 	
 	
